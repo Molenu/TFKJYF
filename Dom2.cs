fccbb2c54e92f28c3d3cb24eeebfd15610c1c8cb
Дарья <120316778+Molenu@users.noreply.github.com>
2023-01-11 13:25:20 +0500

    DDRYTR

---
+++ Dom2.cs
@@ -0,0 +1,16 @@
+﻿ //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры не
+//32679 -> 6
+int dum=new Random().Next(1,1000000);
+Console.WriteLine(dum);
+string Dum1=Convert.ToString(dum);
+if (Dum1.Length>2)
+{
+    Console.WriteLine(Dum1[2]);
+
+}
+else 
+{
+    Console.WriteLine("Третий цифры нету");
+}
---
+++ dom3.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 3 -> 1, 8, 275 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int D=Convert.ToInt32(Console.ReadLine());
+
+for ( int g=1;g<=D ;g++)
+{
+    Console.WriteLine(Math.Pow(g,3));
+}
